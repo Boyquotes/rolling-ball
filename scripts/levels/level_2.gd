55d5cb855fa527a8a5033a4c79ff0b5a63d428c7
ChristianNomena <christiangrzk@gmail.com>
2022-12-04 08:58:15 +0300

    refactor project & add adventure

---
+++ scripts/levels/level_2.gd
@@ -0,0 +1,9 @@
+extends Node3D
+
+
+var level_coins = 0
+
+
+func _on_death_zone_body_entered(body):
+	if body.is_in_group("player"):
+		get_tree().reload_current_scene()
--- scripts/menus/main_menu.gd
+++ scripts/menus/main_menu.gd
@@ -7,14 +7,26 @@ func _ready():
 
 
 func _on_button_adventure_pressed():
-	pass
+	get_tree().change_scene_to_file("res://scenes/chapters/chapter_1.tscn")
 
 
 func _on_button_chill_pressed():
 	get_tree().change_scene_to_file("res://scenes/levels/level_1.tscn")
 
 
+func _on_button_multiplayer_pressed():
+	pass
+
+
 func _on_button_options_pressed():
+	pass
+
+
+func _on_button_help_pressed():
+	pass
+
+
+func _on_button_credits_pressed():
 	pass
 
 
